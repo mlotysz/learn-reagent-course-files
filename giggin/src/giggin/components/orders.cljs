6b0efc962056088a3eb7b69fd733c62d2f62716b
Maciej Łotysz <lotysz@gmail.com>
2022-10-30 00:00:17 +0200

    08. For over orders

--- giggin/src/giggin/components/orders.cljs
+++ giggin/src/giggin/components/orders.cljs
@@ -1,5 +1,21 @@
-(ns giggin.components.orders)
+(ns giggin.components.orders
+  (:require
+    [giggin.state :as state]))
 
 (defn orders
   []
-  [:orders "orders"])
+  [:aside
+   [:div.order
+    [:div.body
+     (doall
+      (for [[id quant] @state/orders]
+        (let [item (get @state/gigs id)
+              {:keys [img title price]} item
+              total (* price quant)]
+          [:div.item {:key id}
+           [:div.img
+            [:img {:src img :alt title}]]
+           [:div.content
+            [:p.title (str title " \u00D7 " quant)]]
+           [:div.action
+            [:div.price total]]])))]]])
