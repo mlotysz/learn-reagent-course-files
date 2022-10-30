94dac63bbfd112aaea89e08af28ac0fe5a63e1be
Maciej Łotysz <lotysz@gmail.com>
2022-10-30 22:15:24 +0100

    14. Gig editor

--- giggin/src/giggin/components/gigs.cljs
+++ giggin/src/giggin/components/gigs.cljs
@@ -1,19 +1,37 @@
 (ns giggin.components.gigs
   (:require [giggin.state :as state]
-            [giggin.helpers :as h]))
+            [giggin.helpers :as h]
+            [giggin.components.gig-editor :as e]
+            [reagent.core :as r]))
 
 (defn gigs
   []
-  [:main
-   [:div.gigs
-    (for [{:keys [id img title price desc]} (vals @state/gigs)]
-      [:div.gig {:key id}
-       [:img.gig__artwork {:src img :alt title}]
-       [:div.gig__body
-        [:div.gig__title
-         [:div.btn.btn--primary.float--right.tooltip
-          {:data-tooltip "Add to order"
-           :on-click #(swap! state/orders update id inc)}
-          [:i.icon.icon--plus]] title
-         [:p.gig__price (h/format-price price)]
-         [:p.gig__desc desc]]]])]])
+  (let [modal (r/atom false)
+        values (r/atom {:id nil
+                        :title ""
+                        :desc ""
+                        :img ""
+                        :price 0
+                        :sold-out false})
+        add-to-order #(swap! state/orders update % inc)]
+    (fn
+      []
+      [:main
+       [:div.gigs
+        [:button.add-gig
+         {:on-click #(reset! modal true)}
+         [:div.add__title
+          [:i.icon.icon--plus]
+          [:p "Add gig"]]]
+        [e/gig-editor modal values]
+        (for [{:keys [id img title price desc]} (vals @state/gigs)]
+          [:div.gig {:key id}
+           [:img.gig__artwork {:src img :alt title}]
+           [:div.gig__body
+            [:div.gig__title
+             [:div.btn.btn--primary.float--right.tooltip
+              {:data-tooltip "Add to order"
+               :on-click #(swap! state/orders update id inc)}
+              [:i.icon.icon--plus]] title
+             [:p.gig__price (h/format-price price)]
+             [:p.gig__desc desc]]]])]])))
